452e7ef8e03f4ab10204e3d9303bda2272d981f4
Kevin Retzke <kretzke@fnal.gov>
2021-03-12 23:16:40 +0000

    add complex_series query with nested time path

--- src/graphql_test_source/schema.clj
+++ src/graphql_test_source/schema.clj
@@ -9,9 +9,6 @@
            (java.time.temporal ChronoUnit)
            (java.time.format DateTimeParseException)))
 
-(defn random-step [v]
-  (+ v (rand) -0.5))
-
 (defn timestamp->instant
   "Parses timestamp to java.time.Instant instance.
   Handles:
@@ -40,26 +37,11 @@
                     (string? zone) (ZoneId/of zone)
                     :else (ZoneId/systemDefault))))))))
 
-(defn time-step [d t]
-  (.plusMillis t d))
-
-(defn resolve-simple-series
-  [context args value]
-  (let [{:keys [from to interval]} args
-        step (partial time-step interval)
-        t (atom (timestamp->instant from))
-        end (timestamp->instant to)
-        v (atom (* (rand) 100.0))]
-    (cons {:timestamp (.toString @t) :value @v}
-          (repeatedly (/ (.until @t end ChronoUnit/MILLIS) interval)
-                      (fn [] {:timestamp (.toString (swap! t step))
-                              :value (swap! v random-step)})))))
-
 (defn step [interval {:keys [timestamp value]}]
   {:timestamp (.plusMillis timestamp interval)
-   :value (random-step value)})
+   :value (+ value (rand) -0.5)})
 
-(defn resolve-simple-series2
+(defn resolve-simple-series
   [context args value]
   (let [{:keys [from to interval_ms]} args
         from (timestamp->instant from)
@@ -70,9 +52,26 @@
                (iterate (partial step interval_ms)
                         {:timestamp from :value (* (rand) 100.0)})))))
 
+(defn resolve-complex-series
+  [context args value]
+  (let [{:keys [from to interval_ms]} args
+        from (timestamp->instant from)
+        to (timestamp->instant to)
+        span (.until from to ChronoUnit/MILLIS)]
+    (map (fn [x]
+           (assoc {}
+                  :group "foo"
+                  :value (:value x)
+                  :value_list (repeatedly 5 #(assoc {} :value (rand)))
+                  :time {:timestamp (.toString (:timestamp x))}))
+           (take (/ span interval_ms)
+                 (iterate (partial step interval_ms)
+                          {:timestamp from :value (* (rand) 100.0)})))))
+
 (defn resolver-map
   []
-  {:query/simple-series resolve-simple-series2})
+  {:query/simple-series resolve-simple-series
+   :query/complex-series resolve-complex-series})
 
 (defn load-schema
   []
